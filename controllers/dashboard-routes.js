2f8667dbbfc84b0edb7ebbdd8b74c36fda7204e8
Father Shark <11543424+brouiller@users.noreply.github.com>
2021-10-26 17:32:45 -0400

    added dashboard and home routes

---
+++ controllers/dashboard-routes.js
@@ -0,0 +1,49 @@
+const router = require("express").Router();
+const { Post } = require("../models/");
+const withAuth = require("../utils/auth");
+
+router.get("/", withAuth, async (req, res) => {
+  try {
+    const userPosts = await Post.findAll({
+      where: {
+        userId: req.session.userId,
+      },
+    });
+
+    const posts = userPosts.map((post) => post.get({ plain: true }));
+
+    res.render("all-posts-admin", {
+      layout: "dashboard",
+      posts,
+    });
+  } catch (err) {
+    res.redirect("login");
+  }
+});
+
+router.get("/new", withAuth, (req, res) => {
+  res.render("new-post", {
+    layout: "dashboard",
+  });
+});
+
+router.get("/edit/:id", withAuth, async (req, res) => {
+  try {
+    const editPost = await Post.findByPk(req.params.id);
+
+    if (editPost) {
+      const post = editPost.get({ plain: true });
+
+      res.render("edit-post", {
+        layout: "dashboard",
+        post,
+      });
+    } else {
+      res.status(404).end();
+    }
+  } catch (err) {
+    res.redirect("login");
+  }
+});
+
+module.exports = router;
